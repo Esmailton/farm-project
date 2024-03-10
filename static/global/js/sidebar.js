12b30de25f0bcaf1b1fd6bb7804a1c59cb908920
Esmailton Gomes <contato.esmailtongomes@gmail.com>
2024-03-09 23:10:59 -0300

    CHORE: Up sidebar

--- static/global/js/sidebar.js
+++ static/global/js/sidebar.js
@@ -1,3 +1,18 @@
-document.querySelector("#open-btn").addEventListener('click', function(){
-    document.querySelector("#sidebar").classList.toggle('open-sidebar');
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++){
+    arrow[i].addEventListener("click", (e)=>{
+        let arrowParent = e.target.parentElement.parentElement;
+        console.log(arrowParent)
+        arrowParent.classList.toggle("showMenu");
+    });   
+}
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+
+sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
 });
+
+console.log(sidebar)
